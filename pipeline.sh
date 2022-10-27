4b15359286a66f44eaeb570457ddfd82cec50743
lauromoraes <lauromoraes@ufop.edu.br>
2022-10-27 18:59:59 +0000

    new experiments

--- pipeline.sh
+++ pipeline.sh
@@ -3,66 +3,62 @@
 
 # sed -i -e 's/\r$//' pipeline.sh # Correcao de command not found
 
-PARAMS=$1; # Parameters file path
+MYPARAMS=$1; # Parameters file path
 ENV=$2; # Virtual Enviroment name or source path
 
 EXPERIMENT="${1##*/}";	# Remove prefix path
 EXPERIMENT="${EXPERIMENT%%.*}";	# Remove extension
 EXPERIMENTFOLDER="${PWD}/experiments/${EXPERIMENT}";
-PARAMS="${EXPERIMENTFOLDER}/${EXPERIMENT}.yaml"
+#MYPARAMS="${EXPERIMENTFOLDER}/${EXPERIMENT}.yaml"
 
-BASEURL="https://raw.githubusercontent.com/bioinfonupeb/redemicro-miliane-nutri/main/steps";
+BASEURL="https://raw.githubusercontent.com/lauromoraes/microbiom/main/nb-templates";
 
 # Define pipeline steps
 STEPS=(
-	# "step-01-prepare-data"
-	# "step-02-dada2"
-	"step-metataxonomy"
+	"step-prepare-data"
+	"step-quality-control"
 	"step-rarefaction-analysis"
+	"step-metataxonomy"
 	"step-diversity-analysis"
 	);
 
-STEPSDIR="nb-steps"
+STEPSDIR="nb-templates"
 if ! [ -d "$STEPSDIR" ]; then
-  echo "Creating directory: ${STEPSDIR}";
+  echo "Creating directory for notebook templates: ${STEPSDIR}";
   mkdir -p ${STEPSDIR};
+fi
+
+if ! [ -d "$EXPERIMENTFOLDER" ]; then
+  echo "Creating directory for experiment artifacts: ${EXPERIMENTFOLDER}";
+  mkdir -p ${EXPERIMENTFOLDER};
+  cp ${1} ${EXPERIMENTFOLDER}; # Copy pamameters YAML file to experiment
 fi
 
 EXECUTEDDIR="${EXPERIMENTFOLDER}/nb-executed-steps"
 if ! [ -d "$EXECUTEDDIR" ]; then
-  echo "Creating directory: ${EXECUTEDDIR}";
+  echo "Creating directory for executed notebooks: ${EXECUTEDDIR}";
   mkdir -p ${EXECUTEDDIR};
 fi
 
-# Download utils.py file
-if ! [ -f "${STEPSDIR}/utils.py" ]; then
-	echo "Downloading: ${STEPSDIR}/utils.py";
-	wget "${BASEURL}/utils.py" -O "${STEPSDIR}/utils.py";
-	cp "${BASEURL}/utils.py" "${EXECUTEDDIR}/utils.py"
-	cp "${BASEURL}/utils.py" "."
-fi
-
-
-if ! [ -d "$EXPERIMENTFOLDER" ]; then
-  echo "Creating directory: ${EXPERIMENTFOLDER}";
-  mkdir -p ${EXPERIMENTFOLDER};
-  cp ${1} ${EXPERIMENTFOLDER};
-fi
-
-if ! [ -f "${PARAMS}" ]; then
-  cp "${1}" "${PARAMS}";
-fi
+# # Download utils.py file
+# if ! [ -f "${STEPSDIR}/utils.py" ]; then
+# 	echo "Downloading: ${STEPSDIR}/utils.py";
+# 	wget "${BASEURL}/utils.py" -O "${STEPSDIR}/utils.py";
+# 	cp "${BASEURL}/utils.py" "${EXECUTEDDIR}/utils.py"
+# 	cp "${BASEURL}/utils.py" "."
+# fi
 
 
 
-echo "Processing paramenters: ${PARAMS}";
+echo "Processing parameters from: ${MYPARAMS}";
 
 # Activate virtual environment with all dependences
+source ~/anaconda3/etc/profile.d/conda.sh;
 conda activate ${ENV};
 
 # Execute each step
 for i in "${!STEPS[@]}"; do
-	echo "STEP $((i+1)): ${STEPS[i]}";
+	echo "====== Executing Pipeline STEP $((i+1)): ${STEPS[i]} ======";
 
 	# Define paths
 	STEPFILE="${STEPSDIR}/${STEPS[i]}.ipynb";
@@ -70,12 +66,13 @@ for i in "${!STEPS[@]}"; do
 
 	# Download notebook if it not exists
 	if ! [ -f "$STEPFILE" ]; then
-		echo "Downloading: ${STEPFILE}";
+		echo "... Downloading file: ${STEPFILE} ...";
 		wget "${BASEURL}/${STEPS[i]}.ipynb" -O "${STEPSDIR}/${STEPS[i]}.ipynb";
 	fi
 
 	# Execute notebook
-	papermill "${STEPFILE}" "${EXECUTEDFILE}" -f "${PARAMS}";
+	echo ">>> Executing STEP file: ${STEPFILE} <<<";
+	papermill "${STEPFILE}" "${EXECUTEDFILE}" -f "${MYPARAMS}";
 done
 
 # Deactivate the virtual environment
